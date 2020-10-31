3c9320609d30f60b1497b864fbaf35061cf48358
Egor Pavlovich <ambidexterdexter@gmail.com>
2020-10-31 18:32:17 +0300

    #TestTask-5 Изменение файла MainWindow.xaml.cs
    Установление контекста данных для данного окна в виде объекта ApplicationViewModel, который свяжет представление и модели Folder

--- TestTask_WpfApp/MainWindow.xaml.cs
+++ TestTask_WpfApp/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using TestTask_WpfApp.MVVM;
+
 namespace TestTask_WpfApp
 {
     /// <summary>
@@ -23,6 +25,8 @@ namespace TestTask_WpfApp
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = new ApplicationViewModel();
         }
     }
 }
