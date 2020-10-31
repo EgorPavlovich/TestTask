64553779b0f6ae7909f9d54ac43e5896cf002903
Egor Pavlovich <ambidexterdexter@gmail.com>
2020-10-31 18:18:45 +0300

    #TestTask-3 Изменения в классе ApplicationViewModel
    Добавление кода в файл ApplicationViewModel.cs

--- TestTask_WpfApp/MVVM/ApplicationViewModel.cs
+++ TestTask_WpfApp/MVVM/ApplicationViewModel.cs
@@ -2,9 +2,43 @@
 using System.Collections.Generic;
 using System.Text;
 
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+
 namespace TestTask_WpfApp.MVVM
 {
-    class ApplicationViewModel
+    public class ApplicationViewModel : INotifyPropertyChanged
     {
+        private Folder selectedFolder;
+
+        public ObservableCollection<Folder> Folders { get; set; }
+        public Folder SelectedFolder
+        {
+            get { return selectedFolder; }
+            set
+            {
+                selectedFolder = value;
+                OnPropertyChanged("SelectedFolder");
+            }
+        }
+
+        public ApplicationViewModel()
+        {
+            Folders = new ObservableCollection<Folder>
+            {
+                new Folder { Path = @"D:\", Quantity = 1, SumOfByte = 56000 },
+                new Folder { Path = @"C:\", Quantity = 1, SumOfByte = 34000 },
+                new Folder { Path = @"C:\", Quantity = 1, SumOfByte = 66000 },
+                new Folder { Path = @"D:\", Quantity = 1, SumOfByte = 88000 },
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
     }
 }
