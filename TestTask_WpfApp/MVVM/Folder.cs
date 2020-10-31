60a644794f7b6a8b10dd4a02053eccedc4a8f1b6
Egor Pavlovich <ambidexterdexter@gmail.com>
2020-10-31 18:14:02 +0300

    #TestTask-2 Изменения в классе Folder
    Добавление кода в файл Folder.cs

--- TestTask_WpfApp/MVVM/Folder.cs
+++ TestTask_WpfApp/MVVM/Folder.cs
@@ -2,9 +2,52 @@
 using System.Collections.Generic;
 using System.Text;
 
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
 namespace TestTask_WpfApp.MVVM
 {
-    class Folder
+    public class Folder : INotifyPropertyChanged
     {
+        private string path;
+        private int quantity;
+        private float sumOfByte;
+
+        public string Path
+        {
+            get { return path; }
+            set
+            {
+                path = value;
+                OnPropertyChanged("Path"); // Путь к папке: 
+            }
+        }
+        public int Quantity
+        {
+            get { return quantity; }
+            set
+            {
+                quantity = value;
+                OnPropertyChanged("Quantity"); // Количество файлов в папке: 
+            }
+        }
+
+        public float SumOfByte
+        {
+            get { return sumOfByte; }
+            set
+            {
+                sumOfByte = value;
+                OnPropertyChanged("SumOfByte"); // Сумма значений байт файла в папке:
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
     }
 }
