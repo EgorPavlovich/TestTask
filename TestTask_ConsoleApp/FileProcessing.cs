0995778ee7e8cef14deffe7ad62d77d857068545
Egor Pavlovich <ambidexterdexter@gmail.com>
2020-11-01 18:55:29 +0300

    #TestTask-02 Добавление интерфейса IProcessFiles
    Изменение файла FileProcessing.cs Интерфейс нужен
    для реализации паттерна стратегия

--- TestTask_ConsoleApp/FileProcessing.cs
+++ TestTask_ConsoleApp/FileProcessing.cs
@@ -6,5 +6,12 @@ namespace TestTask_ConsoleApp
 {
     public class FileProcessing
     {
+        public interface IProcessFiles
+        {
+            string Path { get; set; }
+            void Process(string Path);
+            void SaveToXmlFile();
+        }
+
     }
 }
