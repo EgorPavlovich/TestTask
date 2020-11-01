04a166cb24f268b1f0ef69439b90b11daa6a9b69
Egor Pavlovich <ambidexterdexter@gmail.com>
2020-11-01 19:05:08 +0300

    #TestTask-04 Изменения класса ProcessedFile
    Изменения класса ProcessedFile.cs Добавлено:
     три сериализируемых свойства,
     один конструктор по умолчанию (нужен для корректной сериализации),
     один конструктор с тремя параметрами

--- TestTask_ConsoleApp/ProcessedFile.cs
+++ TestTask_ConsoleApp/ProcessedFile.cs
@@ -1,10 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace TestTask_ConsoleApp
 {
-    public class ProcessedFile
+    [Serializable]
+    [DataContract]
+    public class ProcessedFile // Класс для сериализации
     {
+        [DataMember]
+        public string FileName { get; set; }
+        [DataMember]
+        public string FullPath { get; set; }
+        [DataMember]
+        public string ByteValues { get; set; }
+
+        public ProcessedFile() { } // Конструктор по умолчанию (нужен для корректной сериализации)
+
+        public ProcessedFile(string FileName, string FullPath, string ByteValues)
+        {
+            this.FileName = FileName;
+            this.FullPath = FullPath;
+            this.ByteValues = ByteValues;
+        }
     }
 }
